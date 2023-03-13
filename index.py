d00cb345a4fb64c93cd375b76dedb59c93249d24
shaheer-2000 <shaheer.2000@hotmail.com>
2023-03-13 17:47:26 +0500

    Add CricketReviews

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 import os
 import re
+from turtle import pos
 from nltk.stem.porter import *
 
 stemmer = PorterStemmer()
@@ -23,10 +24,13 @@ class PositionalNode:
 	
 	def addPosting(self, docID, index):
 		docID = int(docID)
-		if docID not in self.docIndex:
-			self.docIndex[docID] = set()
 
-		self.docIndex[docID].add(index)
+		if docID not in self.docIndex:
+			self.docIndex[docID] = list()
+
+		if index not in self.docIndex[docID]:
+			self.docIndex[docID].append(index)
+			self.docIndex[docID] = sorted(self.docIndex[docID])
 
 	def getPostings(self, docID = None):
 		if docID is None:
@@ -34,18 +38,21 @@ class PositionalNode:
 		else:
 			return sorted(self.docIndex[docID])
 
-abstracts = os.listdir('./Assignment-1/Abstracts')
+# DOCS_PATH = "./Assignment-1/Abstracts"
+DOCS_PATH = "./Assignment-1-Updated/CricketReviews/Dataset"
+STOPWORDS_PATH = "./Assignment-1-Updated/Stopword-List.txt"
+abstracts = os.listdir(DOCS_PATH)
 abstractData = {}
 termDict = {}
 positionalIndex = {}
 
 for abstract in abstracts:
-	with open('./Assignment-1/Abstracts/' + abstract, 'r') as f:
+	with open(DOCS_PATH + "/" + abstract, 'r') as f:
 		abstractData[abstract.split(sep = '.')[0]] = f.read()
 
 stopWords = []
 
-with open('./Assignment-1/Stopword-List.txt', 'r') as f:
+with open(STOPWORDS_PATH, 'r') as f:
 	for s in f:
 		s = re.sub(r'[^A-Za-z]', '', s).lower()
 		if len(s) > 0:
@@ -57,6 +64,8 @@ for a in abstractData:
 	cleanedData = re.sub(r'[^A-Za-z\s]', '', data).lower()
 	
 	tokens = cleanedData.split(' ')
+	# if a == "1":
+		# print(len(tokens), list(set(tokens)))
 
 	for index, token in enumerate(tokens):
 		token = stemmer.stem(token)
@@ -80,14 +89,9 @@ for a in abstractData:
 
 		termDict[token].addPosting(a)
 		positionalIndex[token].addPosting(a, index)
-
-q = input()
-qq = q.split(' ')
-p1 = None
-p2 = None
-i = 0
-
-queryTokens = []
+	
+	# if a == "1":
+	# 	print(len(termDict.keys()), list(set(termDict.keys())))
 
 def diffDoc(list1):
 	list2 = []
@@ -116,28 +120,96 @@ def getIntersect(list1, list2):
 
 	return intersectedList
 
+def getPostingByPosition(term1, term2, pNum):
+	p1 = positionalIndex[term1].getPostings()
+	p2 = positionalIndex[term2].getPostings()
+	list1 = list(p1.keys())
+	list2 = list(p2.keys())
+
+	intersectedList = []
+
+	i = 0
+	j = 0
+	ii = 0
+	jj = 0
+
+	while i < len(list1) and j < len(list2):
+		if list1[i] < list2[j]:
+			i += 1
+		elif list1[i] > list2[j]:
+			j += 1
+		else:
+			while ii < len(p1[list1[i]]) and jj < len(p2[list2[j]]):
+				diff = p1[list1[i]][ii] - p2[list2[j]][jj]
+				# print(diff, list1[i], p1[list1[i]], p2[list2[j]])
+				if diff > 0 and diff <= pNum:
+					intersectedList.append(list1[i])
+					# print('DD')
+					break
+				elif diff > 0:
+					jj += 1
+				else:
+					ii += 1
+			
+			ii = 0
+			jj = 0
+			i += 1
+			j += 1
+
+	return intersectedList
+
 def getUnion(list1, list2):
 	return list(set(list1 + list2))
 
-prevPostingList = None
+while (True):
+	q = input()
+	if q == "q":
+		break
+	qq = q.split(' ')
+	p1 = None
+	p2 = None
+	i = 0
+	prevPostingList = None
 
-while (i < len(qq)):
-	t = qq[i]
+	queryTokens = []
+	while (i < len(qq)):
+		t = qq[i]
 
-	if t == "NOT":
-		prevPostingList = diffDoc(termDict[stemmer.stem(qq[i + 1])].getPostingList())
-		i += 2
-	elif t == "AND":
-		prevPostingList = getIntersect(prevPostingList, termDict[stemmer.stem(qq[i + 1])].getPostingList())
-		i += 2
-	elif t == "OR":
-		prevPostingList = getUnion(prevPostingList, termDict[stemmer.stem(qq[i + 1])].getPostingList())
-		i += 2
-	else:
-		prevPostingList = termDict[stemmer.stem(t)].getPostingList()
-		i += 1
+		if t == "NOT":
+			prevPostingList = diffDoc(termDict[stemmer.stem(qq[i + 1])].getPostingList())
+			i += 2
+		elif t == "AND":
+			prevPostingList = getIntersect(prevPostingList, termDict[stemmer.stem(qq[i + 1])].getPostingList())
+			i += 2
+		elif t == "OR":
+			prevPostingList = getUnion(prevPostingList, termDict[stemmer.stem(qq[i + 1])].getPostingList())
+			i += 2
+		else:
+			# p q \2 OR p
+			# p AND p q \2 -> evaluate p q \2 first, then p AND result of p q \2
+			# if consecutive token, assume proximity
+			prevPostingList = termDict[stemmer.stem(t)].getPostingList()
+			if (i + 1) < len(qq) and qq[i + 1] != "NOT" and qq[i + 1] != "AND" and qq[i + 1] != "OR":
+				wordsForProxQuery = []
+				while (i < len(qq) and not qq[i][0] == "/"):
+					wordsForProxQuery.append(qq[i])
+					i += 1
 
-print(prevPostingList)
+				# /2 means at max 2 words b/w terms, (0) neural (1) x (2) y (3) information
+				proximity = int(qq[i][1:]) + 1 # /2 => 2
+
+				k = 0
+				while k < len(wordsForProxQuery):
+					cPostingList = getPostingByPosition(stemmer.stem(wordsForProxQuery[k]), stemmer.stem(wordsForProxQuery[k + 1]), proximity)
+					k += 2
+					if prevPostingList is None:
+						prevPostingList = cPostingList
+					else:
+						prevPostingList = getIntersect(prevPostingList, cPostingList)
+
+			i += 1
+
+	print(prevPostingList)
 	
 	
 
